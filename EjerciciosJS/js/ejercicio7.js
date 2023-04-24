0680045f7fa5e8b4d786b24f389d9d9a791a6e28
José Luis <joseluis.glezolms@gmail.com>
2023-04-24 18:57:12 +0200

    mod EJ 7 JS

--- EjerciciosJS/js/ejercicio7.js
+++ EjerciciosJS/js/ejercicio7.js
@@ -2,9 +2,9 @@ var pieza1 ={
     peso:"20"}
 
 var pieza2 ={
-    peso:"20"}
+    peso:20}
 
 console.log(pieza1.peso == pieza2);
 console.log(pieza1.peso === pieza2);
 
-console.log((pieza1.peso == 20) ? Number(pieza1.peso) + Number(pieza2.peso) : Number(pieza1.peso) * Number(pieza2.peso));
+console.log((pieza1.peso == "20") ? Number(pieza1.peso) + Number(pieza2.peso) : Number(pieza1.peso) * Number(pieza2.peso));
