3c3cd1046417cfabc8b37faef102fef15b450da5
José Luis <joseluis.glezolms@gmail.com>
2023-04-24 19:32:35 +0200

    added EJ 8 JS

--- EjerciciosJS/js/ejercicio7.js
+++ EjerciciosJS/js/ejercicio7.js
@@ -7,4 +7,4 @@ var pieza2 ={
 console.log(pieza1.peso == pieza2);
 console.log(pieza1.peso === pieza2);
 
-console.log((pieza1.peso == "20") ? Number(pieza1.peso) + Number(pieza2.peso) : Number(pieza1.peso) * Number(pieza2.peso));
+console.log((pieza1.peso == "20") ? pieza1.peso + Number(pieza2.peso) : Number(pieza1.peso) * Number(pieza2.peso));
