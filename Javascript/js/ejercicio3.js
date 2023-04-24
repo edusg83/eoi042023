303712ec408ca512fc4f9c07dbbf686976747e56
mycodedd <ismycod.edd@gmail.com>
2023-04-24 17:41:03 +0200

    jh

--- Javascript/js/ejercicio3.js
+++ Javascript/js/ejercicio3.js
@@ -1,13 +1,15 @@
-</DOCTYPE html>
-<html lang="es" dir="Itr">
-    <head>
-        <title>Bootstrap</title>
-        <meta charset="utf-g">
-        <link rel="stylesheet" type="text/css" href="">
-    </head>
-    <body>
-        <main>
-            
-        </main>  
-    </body>
-</html>
+var cadena1 = "CADENA1";
+
+console.log(cadena1);
+
+console.log(cadena1.indexOf("ENA"));
+
+console.log(cadena1.charAt(cadena1.length-1));
+
+console.log(cadena1.toLocaleLowerCase());
+
+console.log(cadena1.replace("DE", "ME"));
+
+var cadena2 = "CADENA2";
+
+console.log(cadena2+cadena1);
---
+++ Javascript/js/prueba.js
@@ -0,0 +1,16 @@
+var sema = null;
+
+console.log(sema === null);
+console.log(sema == nulll);
+console.log(sema == undefined);
+
+
+var sema = undefined
+
+console.log(sema == undefined);
+console.log(sema === undefined);
+
+console.log(typeof(sema) == "undefined");
+console.log(typeof(sema) == undefined);
+console.log(typeof(sema) === "undefined");
+console.log(typeof(sema) === undefined);
