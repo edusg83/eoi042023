89a80767a6bd45ad9ee5888bbdf7be0ecedd088a
Fran <franlopezcuevasgmail.com>
2023-04-24 16:21:19 +0200

    Ejercicio3

---
+++ JS/Ejercicio3.js
@@ -0,0 +1,29 @@
+// Ejercicio 3
+
+let cadena1 = "CADENA1";
+
+console.log("La variable cadena1 es:", cadena1);
+
+let posicion = cadena1.indexOf("ENA");
+
+console.log("La posición del texto 'ENA' en cadena1 es:", posicion);
+
+let ultimoCaracter = cadena1.charAt(cadena1.length - 1);
+
+console.log("El último carácter de cadena1 es:", ultimoCaracter);
+
+let minusculas = cadena1.toLowerCase();
+
+console.log("La variable cadena1 en minúsculas es:", minusculas);
+
+let sustituida = cadena1.replace("DE", "ME");
+
+console.log("La cadena1 con 'DE' sustituida por 'ME' es:", sustituida);
+
+let cadena2 = "CADENA2";
+
+console.log("La variable cadena2 es:", cadena2);
+
+let concatenada = cadena2.concat(cadena1);
+
+console.log("La concatenación de cadena2 y cadena1 es:", concatenada);
