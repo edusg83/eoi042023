3a1463539afa8fb09ded547051a6827f06adbdc2
Fernando <fernandoramirez199543@gmail.com>
2023-04-24 20:18:06 +0200

    Add Ejercicio10

---
+++ EjerciciosJavaScript/js/ejercicio10.js
@@ -0,0 +1,13 @@
+funciones = {
+    verde: function colorVerde() {
+        console.log("VERDE");
+    },
+
+    rojo: function colorRojo() {
+        console.log("ROJO");
+    }
+}
+
+funciones.rojo();
+
+funciones.verde();
