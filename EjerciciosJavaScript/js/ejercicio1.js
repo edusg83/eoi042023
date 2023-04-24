7c6900cca95406bd28481e4245f3c96fc94b4280
Fernando <fernandoramirez199543@gmail.com>
2023-04-24 15:35:50 +0200

    Add Ejercicio1

---
+++ EjerciciosJavaScript/js/ejercicio1.js
@@ -0,0 +1,6 @@
+console.time("test1");
+console.info("Mensaje de informacion");
+console.warn("Mensaje de alerta");
+console.error("Mensaje de error");
+console.log("Hola Mundo");
+console.timeEnd("test1");
