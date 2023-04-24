7e4cec25edb8aa08343d2d87f54d0edaced12b47
mycodedd <ismycod.edd@gmail.com>
2023-04-24 17:40:54 +0200

    jh

---
+++ Javascript/js/ejercicio5.js
@@ -0,0 +1,14 @@
+//crear una variable de tipo array llamada array1 con valor [1, 2,3,4]
+var array1 = [1, 2, 3, 4];
+
+//Crear un pequeño programa que recorra el array y cuando encuentre el 1 muestre UNO , el 2 muestre DOS, el 3 muestre TRES, el 4 muestre CUATRO. Utilizar la instrucción Switch.
+ for(var i=0; i<length; i++) {
+    switch(i){
+        case 1;
+        console.log("UNO")
+        
+        case 2;
+        
+
+    }
+ }
