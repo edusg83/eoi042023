d665c5b5330f60ba13a94845ce1328517d069c59
Fran <franlopezcuevasgmail.com>
2023-04-24 15:53:51 +0200

    Ejercicio2

---
+++ JS/Ejercicio2.js
@@ -0,0 +1,9 @@
+
+let alto = 12;
+let ancho = 12.203004;
+
+let altoPorAncho = alto * ancho;
+
+console.log("El valor de altoPorAncho es:", altoPorAncho);
+
+console.log("El valor de altoPorAncho redondeado a 2 decimales es:", altoPorAncho.toFixed(2));
