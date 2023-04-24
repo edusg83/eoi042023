949d2d665e14dcb985ca660fc86ef0a290b6733e
Fernando <fernandoramirez199543@gmail.com>
2023-04-24 18:51:49 +0200

    Mod Ejercicio7

--- EjerciciosJavaScript/js/ejercicio7.js
+++ EjerciciosJavaScript/js/ejercicio7.js
@@ -6,9 +6,9 @@ pieza2 = {
     peso : "20",
 }
 
-console.log(typeof(pieza1.peso) == typeof(pieza2.peso));
+console.log(pieza1.peso == pieza2.peso);
 
-console.log(typeof(pieza1.peso) === typeof(pieza2.peso));
+console.log(pieza1.peso === pieza2.peso);
 
 var resultado = pieza1.peso = "20" ? pieza1.peso + pieza2.peso : pieza1.peso * pieza2.peso;
 
