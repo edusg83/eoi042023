ac35a154f2a7b4e2a7631dbd9b73d277fc996271
Fernando <fernandoramirez199543@gmail.com>
2023-04-24 16:22:14 +0200

    Add Ejercicio3

---
+++ EjerciciosJavaScript/js/ejercicio3.js
@@ -0,0 +1,8 @@
+var cadena1 = "CADENA1",
+    cadena2 = cadena1;
+
+console.log(cadena1.indexOf("ENA"));
+console.log(cadena1.charAt(cadena1.length-1));
+console.log(cadena1.toLowerCase());
+console.log(cadena1.replace("DE", "ME"));
+console.log(cadena1 + cadena2);
