4cd8eeeaf740ee178bd219f9cf767b8063c22389
mycodedd <ismycod.edd@gmail.com>
2023-04-24 16:14:11 +0200

    e

---
+++ Javascript/js/ejercicio1.js
@@ -0,0 +1,2 @@
+console.log("prueba", 1, {}, [1,2,3]);
+cnosole
---
+++ Javascript/js/ejercicio3.js
@@ -0,0 +1,13 @@
+</DOCTYPE html>
+<html lang="es" dir="Itr">
+    <head>
+        <title>Bootstrap</title>
+        <meta charset="utf-g">
+        <link rel="stylesheet" type="text/css" href="">
+    </head>
+    <body>
+        <main>
+            
+        </main>  
+    </body>
+</html>
