ffb2559cb307d42a7a9ef16da8f90b22a24d7ae8
Fran <franlopezcuevasgmail.com>
2023-04-24 15:31:22 +0200

    Ejercicio1

---
+++ JS/Ejercicio1.js
@@ -0,0 +1,13 @@
+// Ejercicio 1 JavaScript
+
+console.info("MENSAJE DE INFO");
+
+console.warn("MENSAJE DE WARNING");
+
+console.error("MENSAJE DE ERROR");
+
+console.log("MENSAJE DE LOG");
+
+console.time("Tiempo de ejecución");
+
+console.timeEnd("Tiempo de ejecución");
