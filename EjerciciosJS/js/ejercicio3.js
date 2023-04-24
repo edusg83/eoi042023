b006a53ebd2ab00c12f4eb130ba7e9f3361d444f
José Luis <joseluis.glezolms@gmail.com>
2023-04-24 16:19:51 +0200

    added EJ 3 JS

---
+++ EjerciciosJS/js/ejercicio3.js
@@ -0,0 +1,16 @@
+var cadena1 = "CADENA1";
+
+console.log(cadena1);
+
+console.log(cadena1.indexOf("ENA"));
+
+console.log(cadena1.charAt(cadena1.length-1));
+
+console.log(cadena1.toLocaleLowerCase());
+
+console.log(cadena1.replace("DE","ME"));
+
+var cadena2 = "CADENA2";
+
+console.log(cadena2+cadena1);
+
