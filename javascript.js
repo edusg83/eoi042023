6c67a845e1e89b6ffffaa106bb6937fb70af7cac
edusg83 <edusg83@gmail.com>
2023-04-24 19:37:17 +0200

    anónimas

--- javascript.js
+++ javascript.js
@@ -1,2 +1,13 @@
-let edad=39;
-console.log(typeof(edad));
+
+
+
+(function(uno, dos, tres){
+    console.log(uno)
+    console.log(dos)
+    console.log(tres)
+}(1,2,3));
+
+(function(){
+    console.log("función anónima");
+})
+
