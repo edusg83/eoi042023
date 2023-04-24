f2a0ba6fb6922cb0125a470bcd84cbc8c3ee1134
Fernando <fernandoramirez199543@gmail.com>
2023-04-24 17:33:16 +0200

    Add Ejercicio5

---
+++ EjerciciosJavaScript/js/ejercicio5.js
@@ -0,0 +1,28 @@
+var array1 = [1,2,3,4];
+
+function valor() {
+    for (var indice of array1) {
+
+        switch (indice) {
+            case 1: 
+                console.log("UNO");
+                break;
+            case 2: 
+                console.log("DOS");
+                break;
+            case 3: 
+                console.log("TRES");
+                break;
+            case 4: 
+                console.log("CUATRO");
+                break;
+        }
+        
+    }
+}
+
+valor();
+
+array1.push[3]
+
+valor();
