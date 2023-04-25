a786544e80a1cb6c201af2bc6cb1a1af747aed71
José Luis <joseluis.glezolms@gmail.com>
2023-04-25 17:29:36 +0200

    added EJ 14 JS

--- EjerciciosJS/js/ejercicio13.js
+++ EjerciciosJS/js/ejercicio13.js
@@ -2,5 +2,4 @@ try{
     throw new EvalError("Se ha producido un error", "ejercicio13.js", 100)
 }catch(error){
     console.log(error.message)
-
 }
---
+++ EjerciciosJS/js/ejercicio14.js
@@ -0,0 +1,26 @@
+let objetoPadre = {
+    edad: null,
+    nombre: null,
+    apellidos: null,
+    muestraDatos: function () {
+        console.log(this.nombre + " " + this.apellidos + " tiene una edad de " + this.edad);
+    }
+};
+
+let hijo1 = Object.create(objetoPadre);
+hijo1.edad = 21;
+hijo1.nombre = 'Antonio';
+hijo1.apellidos = 'Martinez Garcia';
+
+let hijo2 = Object.create(objetoPadre);
+hijo2.edad = 30;
+hijo2.nombre = 'Pepe';
+hijo2.apellidos = 'Martinez Garcia';
+hijo1.muestraDatos();
+hijo2.muestraDatos();
+
+objetoPadre.verEdad = function () {
+    console.log(this.edad);
+}
+hijo1.verEdad();
+hijo2.verEdad();
