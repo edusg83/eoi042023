2005a15c0941791f0f9cd9afe2c22bea009715ac
mycodedd <ismycod.edd@gmail.com>
2023-04-25 19:08:32 +0200

    f

---
+++ Javascript/js/ejercicio18.js
@@ -0,0 +1,21 @@
+//b)
+let headerId=function(){
+    document.getElementById("header");
+    console.log(headerId);
+};
+
+headerId;
+
+let headerTag=function(){
+    document.getElementsByTagName("header");
+    console.log(headerTag)
+};
+
+headerTag;
+
+let headerQuery=function(){
+    document.querySelector("header");
+    console.log(headerQuery);
+}
+
+headerQuery;
