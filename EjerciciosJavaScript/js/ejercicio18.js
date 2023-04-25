83d4a785841ecb83b1e0a4536b3d6129b3f67cc8
Fernando <fernandoramirez199543@gmail.com>
2023-04-25 19:08:13 +0200

    Add Ejercicio18

---
+++ EjerciciosJavaScript/js/ejercicio18.js
@@ -0,0 +1,7 @@
+var elementsTagName = document.getElementsByTagName("header");
+var elementId = document.getElementById("cabecera");
+var elementsQuery = document.querySelector("#cabecera");
+
+console.log(elementsTagName);
+console.log(elementId);
+console.log(elementsQuery);
