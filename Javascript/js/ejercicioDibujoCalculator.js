de1f7e7f1aa443d79359698519f7c0c3b2b9b165
mycodedd <ismycod.edd@gmail.com>
2023-04-26 00:35:57 +0200

    f

---
+++ Javascript/js/ejercicioDibujoCalculator.js
@@ -0,0 +1,41 @@
+//division
+function dividir (param1, param2){
+    return param1/param2;
+};
+
+//multiplicacion
+function multiplicar (param1, param2){
+    return param1*param2;
+};
+
+//resta
+function restar (param1, param2){
+    return param1-param2;
+};
+
+//suma
+function sumar (param1, param2){
+    return param1+param2;
+};
+
+//igual
+function resultado(){
+    switch(operacion){
+        case dividir:
+            console.log(dividir());
+            break;
+        case multiplicar:
+            console.log(multiplicar());
+            break;
+        case restar:
+            console.log(restar());
+            break;
+        case sumar:
+            console.log(sumar());
+            break;
+    }
+}
+
+console.log(dividir(4,2)+resultado);
+
+//decimal
