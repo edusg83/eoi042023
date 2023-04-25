d1d1ea26853ad35307696aebbf50b8db39b16d75
mycodedd <ismycod.edd@gmail.com>
2023-04-25 13:50:11 +0200

    ejercicio3

--- Javascript/js/ejercicio3.js
+++ Javascript/js/ejercicio3.js
@@ -1,15 +1,40 @@
-var cadena1 = "CADENA1";
+//COMPROBACION 1
+// var cad ="" , cad2='';
+// var cadena2="Esto es una 'cadena'";
+// var cadena3='Y "esto" también';
 
-console.log(cadena1);
+// console.log(cad2.length + ", " + cadena2.length);
+// console.log(cadena3 + " / El tamaño de esta cadena es de: " + cadena3.length + " / " + "El índice de 'esto' es: " + cadena3.indexOf("esto"));
 
+//COMPROBACION 2
+// var str1="12";
+// var str2=12
+// console.log(str1==str2);
+// console.log(str1 === str2);
+// console.log(Number(str1)===str2);
+
+//COMPROBACION 3
+// var nombre ="El café estaba muy rico cuando 'estaba' en Colombia";
+// //console.log(nombre.indexOf("'estaba'")); ==> indice 31
+// nombre=nombre.replace("'estaba'", "estuve");
+// console.log(nombre);
+
+
+
+//Ejercicio3
+//a)
+var cadena1 ="CADENA1";
+//b)
 console.log(cadena1.indexOf("ENA"));
+//c)
+console.log(cadena1.charAt(6));
+//d)
+console.log(cadena1.toLowerCase());
+//e)
+cadena1=cadena1.replace("DE", "ME");
+console.log(cadena1);
+//f)
+var cadena2= "CADENA2";
+console.log(cadena2 + ", " + cadena1);
 
-console.log(cadena1.charAt(cadena1.length-1));
 
-console.log(cadena1.toLocaleLowerCase());
-
-console.log(cadena1.replace("DE", "ME"));
-
-var cadena2 = "CADENA2";
-
-console.log(cadena2+cadena1);
