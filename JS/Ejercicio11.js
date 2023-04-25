098512f198c2504a322d5866816503feb3083a66
edusg83 <edusg83@gmail.com>
2023-04-25 17:11:51 +0200

    try

---
+++ JS/Ejercicio11.js
@@ -0,0 +1,13 @@
+
+var funciones = {
+    colorVerde: function() {
+      console.log("VERDE");
+    },
+    colorRojo: function() {
+      console.log("ROJO");
+      this.colorVerde(); 
+    }
+  };
+  
+  funciones.colorRojo(); 
+  
