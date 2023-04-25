1fffe443ff126fe78662b1a92406614f195d6fcb
mycodedd <ismycod.edd@gmail.com>
2023-04-25 15:26:27 +0200

    f

---
+++ Javascript/js/ejercicio4.js
@@ -0,0 +1,18 @@
+// COMPROBACION 1 (Conclusion: no se como pasarle esto a la consola, pero entendido)
+// var count;
+// function inc(){
+//     count++;
+//     var message="La cuenta es " + count;
+
+//     alert(message);
+// }
+
+// COMPROBACION 2 (Conclusion: mismo valor, pero no mismo tipo)
+// var casoA;
+// var casoB=null;
+// var casoC=casoB===casoA;
+// var casoD=casoB==casoA;
+
+// console.log(casoD);
+
+
