3a5ae751b2317a95cc6b7b2701a4e3429ece92e5
Fran <franlopezcuevasgmail.com>
2023-04-25 18:56:25 +0200

    j

---
+++ JS/Ejercicio18.js
@@ -0,0 +1,9 @@
+let headers = document.getElementsByTagName("header");
+console.log(headers[0]);
+
+let header = document.getElementById("header");
+console.log(header);
+
+let header2 = document.querySelector("header");
+console.log(header2);
+
