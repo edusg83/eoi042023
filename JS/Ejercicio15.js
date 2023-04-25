4a9543fd0799e088a080243f5a9fcec9cc7165a0
Fran <franlopezcuevasgmail.com>
2023-04-25 18:15:58 +0200

    j

---
+++ JS/Ejercicio15.js
@@ -0,0 +1,8 @@
+let nombre = prompt("Introduzca el nombre:");
+console.log("El valor introducido es: " + nombre);
+
+let continuar = confirm("¿Desea continuar?");
+console.log("El valor de la confirmación es: " + continuar);
+
+
+
