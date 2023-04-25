291502b57d87072d7ebafcb4f637d87221102235
mycodedd <ismycod.edd@gmail.com>
2023-04-25 12:21:47 +0200

    ejercicio2

---
+++ Javascript/js/ejercicio2.js
@@ -0,0 +1,13 @@
+//a)
+var alto = 12;
+var ancho = new Number(12.203004);
+
+//b)
+var altoPorAncho = alto*ancho;
+
+//c)
+console.log("El resultado es: " + altoPorAncho);
+
+//d)
+console.log("El resultado con 'toFixed' es: " + altoPorAncho.toFixed(2));
+
