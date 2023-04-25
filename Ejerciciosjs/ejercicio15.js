f045777bc2423ec5caee620986a470999d350bad
domingo <domocu4299@gmail.com>
2023-04-25 18:22:00 +0200

    ej15

---
+++ Ejerciciosjs/ejercicio15.js
@@ -0,0 +1,4 @@
+var nombre = prompt("Introduzca el nombre:");
+console.log("Nombre introducido: " + nombre);
+var continuar = confirm("¿Desea continuar?");
+console.log("Confirmación: " + continuar);
