e81ed4078f412a553721fe22f33865c72f4faea6
domingo <domocu4299@gmail.com>
2023-04-25 19:00:10 +0200

    ej18

---
+++ Ejerciciosjs/ejercicio18.js
@@ -0,0 +1,8 @@
+var header1 = document.getElementsByTagName("header")[0];
+console.log("Usando getElementsByTagName:", header1);
+
+var header2 = document.getElementById("header");
+console.log("Usando getElementById:", header2);
+
+var header3 = document.querySelector("header");
+console.log("Usando querySelector:", header3);
