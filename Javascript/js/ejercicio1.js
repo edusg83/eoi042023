4c9ef8743a24fc33c0f0124f4831013438f46419
mycodedd <ismycod.edd@gmail.com>
2023-04-25 12:04:43 +0200

    ejercicio1

--- Javascript/js/ejercicio1.js
+++ Javascript/js/ejercicio1.js
@@ -1,1 +1,9 @@
-console.log("prueba", 1, {}, [1,2,3]);
+console.info("MENSAJE DE INFO")
+console.warn("MENSAJE DE WARNING");
+console.error("MENSAJE DE ERROR");
+console.log("MENSAJE DE LOG");
+console.time("timer");
+console.timeLog("timer");
+console.timeLog("timer");
+console.timeLog("timer");
+console.timeEnd("timer");
