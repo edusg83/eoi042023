f9fec27fafb082347f20578326a3b812bb83ab8c
José Luis <joseluis.glezolms@gmail.com>
2023-04-26 19:12:48 +0200

    added EJ 21 JS

---
+++ EjerciciosJS/js/ejercicio21.js
@@ -0,0 +1,34 @@
+
+(function() {
+    var elem = document.createElement("p");
+    elem.setAttribute("id","p1")
+    elem.innerHTML = "Parrafo1";
+    document.body.appendChild(elem);
+}());
+
+
+(function() {
+    var elem = document.createElement("button");
+    elem.setAttribute("id","boton1")
+    elem.innerHTML = "Aplicar estilo a parrafo";
+    let pElem = document.getElementById("p1")
+    pElem.appendChild(elem);
+}());
+
+
+let parrafoElem = document.getElementById("p1")
+let botonElem = document.getElementById("boton1")
+
+botonElem.onclick = function(){
+    if(parrafoElem.className == "" ){
+        parrafoElem.classList.add("parrafo")
+        parrafoElem.classList.add("aplicado")
+        
+        botonElem.innerHTML = "Quitar estilo al parrafo"
+    }else{
+        parrafoElem.className = ""
+        botonElem.innerHTML = "Aplicar estilo a parrafo"
+        parrafoElem.classList.remove("parrafo")
+        parrafoElem.classList.remove("aplicado")
+    }
+}
