2ee2f739d33dfcebfd2812e3928ead5cc37b7e50
José Luis <joseluis.glezolms@gmail.com>
2023-04-26 16:07:15 +0200

    added CalcGraf JS

--- EjerciciosJS/js/CalcGrafica.js
+++ EjerciciosJS/js/CalcGrafica.js
@@ -1,4 +1,38 @@
-let pantalla ="";
-function anyadirNumero(num){
-    pantalla +=
+let pantalla = "",
+    memoria = "",
+    operador = "",
+    finOperacion = false,
+    elementoPantalla = document.getElementById("pantalla");
+
+function anyadirNumero(num) {
+
+    pantalla += num
+    elementoPantalla.innerHTML = pantalla
+}
+
+function operar(op) {
+
+    if (memoria === "") {
+        operador = op;
+        memoria = pantalla;
+        pantalla = ""
+        elementoPantalla.innerHTML = operador
+    } 
+}
+
+function calcular() {
+
+    let calculo = (Number(memoria)<0 && operador === "-")? eval(pantalla +memoria): eval(pantalla + operador +memoria)
+    elementoPantalla.innerHTML = calculo
+    memoria = "";
+    pantalla = "";
+}
+
+function negativo(){
+    
+    if(pantalla ===""){
+        anyadirNumero("-")
+    }else if(pantalla !="" && memoria === ""){
+        operar("-")
+    }  
 }
