54fe2ca608417fa3be5465291f0309d51b262189
Fernando <fernandoramirez199543@gmail.com>
2023-04-26 19:08:00 +0200

    Add Ejercicio21

---
+++ EjerciciosJavaScript/js/ejercicio21.js
@@ -0,0 +1,28 @@
+(function() {
+    
+    var parrafo = document.createElement("p");
+    parrafo.innerHTML = "Parrafo1";
+    document.body.appendChild(parrafo);
+    parrafo.id = "p1";
+
+    var boton = document.createElement("button");
+    boton.innerHTML = "Aplicar estilo a párrafo";
+    document.body.appendChild(boton);
+
+
+    boton.onclick = function() {
+        var parrafo = document.getElementById("p1");
+
+        if (parrafo.classList.contains("parrafo")) {
+            parrafo.classList.remove("parrafo")
+            boton.innerHTML = "Aplicar estilo a párrafo";
+            parrafo.classList.remove("aplicado")
+        } else {
+            parrafo.classList.add("parrafo")
+            boton.innerHTML = "Quitar estilo a parrafo";
+            parrafo.classList.add("aplicado")
+        }
+   
+    }
+
+}());
