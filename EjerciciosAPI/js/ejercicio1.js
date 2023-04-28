a660ba355ff66c1dd4b6d4bd14354812d0b2d591
edusg83 <edusg83@gmail.com>
2023-04-28 16:45:18 +0200

    api

--- EjerciciosAPI/js/ejercicio1.js
+++ EjerciciosAPI/js/ejercicio1.js
@@ -7,8 +7,13 @@ const credentials = request.credentials;
 let array = [];
 
 fetch(request)
-.then(Response => Response.json())
-.then(data => 
-    array = data);
+.then(respuesta => respuesta.json())
+.then(data => { 
+    console.log(data);
+    console.log(data.arrayUsuarios);
+    arrayData=data.arrayUsuarios;
+    document.getElementById("nombre").innerHTML=arrayData[0].nombre;
+    document.getElementById("apellidos").innerHTML=arrayData[0].apellidos;
+    document.getElementById("direccion").innerHTML=arrayData[0].direccion;
+});
 
-console.log(array)
