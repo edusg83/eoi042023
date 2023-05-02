9b5f1d0c8326e4e13a6e6da8e9e56b8faef65a47
Fernando <fernandoramirez199543@gmail.com>
2023-05-02 17:06:30 +0200

    Add Productos

---
+++ ConexionAPI/js/productos.js
@@ -0,0 +1,70 @@
+const headers = {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+}
+
+const urlProductos = 'http://ligafalm.eu:28100/products?page=0&size=100';
+
+axios.get(urlProductos, {headers})
+.then((respuesta) => {
+
+    let arrayData = respuesta.data;
+
+    let tabla = `<table id="dataTable" class="table table-bordered dataTable">
+    <thead>
+        <tr>
+            <th>Nombre</th>
+            <th>Descripcion</th>
+            <th>Code</th>
+            <th>Opciones</th>
+        </tr>
+    </thead>
+    <tbody>`;
+
+    let finTabla = `</tbody>
+                    </table>`;
+
+    let filas = ``;
+
+    arrayData.forEach(element => {
+        
+        filas += `
+        <tr>
+            <td>${element.name}</td>
+            <td>${element.description}</td>
+            <td>${element.code}</td>
+            <td>
+                <button class="btn btn-primary btn-circle">
+                    <i  data-bs-toggle="modal" data-bs-target="#modalBorrar" onclick="previoBorrar(${element.id})" class="bi bi-trash"></i>
+                </button>
+                <button class="btn btn-primary btn-circle">
+                    <i  data-bs-toggle="modal" data-bs-target="#modalModificar" onclick="previoModificar(${element.id})" class="bi bi-trash"></i>
+                </button>
+            </td>
+        </tr>`
+    });
+
+    tabla += filas + finTabla;
+    document.getElementById("resultados").innerHTML = tabla;
+
+})
+
+let idProducto = 0;
+
+
+function previoBorrar (id) {
+    console.log("Dentro de la funcion previoBorrar, idProducto = " + id);
+    idProducto = id;
+}
+
+function borrarProducto() {
+    console.log("Dentro de la funcion borrar, idProducto = " + idProducto);
+    axios.delete('http://ligafalm.eu:28100/products/' + idProducto)
+    .then(() => alert("Producto eliminado"));
+}
+
+
+
+
+
+
