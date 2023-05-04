660cb3f66323765f545eef54c0380a5680b8fa4e
José Luis <joseluis.glezolms@gmail.com>
2023-05-04 20:12:19 +0200

    added token fin

--- EjercicioToken/token.js
+++ EjercicioToken/token.js
@@ -7,10 +7,10 @@ passwd: "eoi"}
 2- Almacenar el token en SessionStorage del navegador
 
 3- Decodificar el token y sacar las siguientes variables:
-	- username
-	- email
-	- roles
-	- id
+    - username
+    - email
+    - roles
+    - id
 
 4- Con el identificador obtenido (id) llamar al siguiente endpoint para conseguir la información del usuario
 
@@ -28,46 +28,54 @@ HEADER: token */
 
 let token;
 
-function iniciarSesion(){
-    
-    let user =document.getElementById("usuario").value
-    let pass =document.getElementById("password").value
-    console.log(user)
+function iniciarSesion() {
+
+    let user = document.getElementById("usuario").value
+    let pass = document.getElementById("password").value
+
 
     axios.post('https://eoi.wiremockapi.cloud/token', {
         "username": user,
         "password": pass
     },)
         .then((respuesta) => {
-        sessionStorage.setItem('token',respuesta.data["access-token"]);
-        token = sessionStorage.getItem('token',respuesta.data["access-token"]);
-        console.log("datos del token codificado: "+token)
-        
-        /* DECODIFICAR TOKEN */
-        let base64Token=token.split('.')[1];
-        let base64aux=base64Token.replace('-','+').replace('_','/');
-        let decodedToken = JSON.parse(window.atob(base64aux));
+            sessionStorage.setItem('token', respuesta.data["access-token"]);
+            token = sessionStorage.getItem('token', respuesta.data["access-token"]);
+
+            document.getElementById("info1").innerText = "datos del token codificado: " + token
+
+            /* DECODIFICAR TOKEN */
+            let base64Token = token.split('.')[1];
+            let base64aux = base64Token.replace('-', '+').replace('_', '/');
+            let decodedToken = JSON.parse(window.atob(base64aux));
+
+            let id = decodedToken.id;
+            let username = decodedToken.username;
+            let email = decodedToken.email;
+            let roles = decodedToken.roles;
+
+            document.getElementById("info2").innerText = "datos del token decodificado: "+ 
+            "id: " + id + ", username: " + username + ", email: " + email + ", roles: " + roles 
 
 
-        console.log("datos del token decodificado: " + decodedToken)
-        
-        let id=decodedToken.id;
-        let username = decodedToken.username;
-        let email = decodedToken.email;
-        let roles = decodedToken.roles;
+            // Con el identificador obtenido (id) llamar al siguiente endpoint para conseguir la información del usuario
+            const headers = { 'token': token }
+            let urlId = 'https://eoi.wiremockapi.cloud/usuario/' + id
 
-        console.log("id: "+ id+ ", username: "+username+", email: "+email+", roles: "+roles)
+            axios.get(urlId, { headers })
+                .then((respuestaGet) => {
 
-        console.log(token)
-        const headers={
-            'token':token
-        }
-        // Con el identificador obtenido (id) llamar al siguiente endpoint para conseguir la información del usuario
-        let urlId = 'https://eoi.wiremockapi.cloud/usuario/'+ id
-        axios.get(urlId, {headers})
-            .then((respuesta) => {
-                
-                console.log(respuesta)
-            })
+                    document.getElementById("info3").innerText = "Datos del Usuario devueltos:  "+ respuestaGet.data['email']+",  "+ respuestaGet.data['username']
+
+                    axios.post('https://eoi.wiremockapi.cloud/usuario', {
+                        "email": respuestaGet.data['email'],
+                        "username": respuestaGet.data['username']
+                    },{ headers })
+                        .then((respuestaPost) => {
+                           
+                            document.getElementById("info5").innerText = "Estado: "+ respuestaPost.status
+                        })
+                })
         })
-}
+    }
+
